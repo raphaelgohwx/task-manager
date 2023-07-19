24af9ebfd1e0ff1ec570cdd500abf98781841ca4
Raphael Goh <71965935+raphaelgohwx@users.noreply.github.com>
2023-07-19 16:18:02 +0800

    19/7/2023
    - Completed general styling
    - Included minor QOL improvements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,104 @@
-import AdditionBar from './components/AdditionBar';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  var taskList = []
+    var completedList = []
+    const [tasks, setTasks] = useState(taskList);
+    const [completed, setCompleted] = useState(completedList);
+    const [inputValue, setInputValue] = useState('');
+    const handleChange = (event) => {
+        setInputValue(event.target.value)
+    }
+
+    // const handleRemove = (index) => 
+
+    function handleTRemove(index) {
+        setTasks(tasks.filter((_, i) => i !== index));
+    }
+
+    function handleCRemove(index) {
+        setCompleted(completed.filter((_, i) => i !== index));
+    }
+
+    function handleClick() {
+        setTasks((prevTasks) => [
+            ...prevTasks,
+            inputValue
+        ])
+
+        // Resets input value
+        setInputValue("")
+    }
+
+    function handleDone({ task, index }) {
+        setCompleted((prevTasks) => [
+            ...prevTasks,
+            task
+        ])
+
+        handleTRemove(index)
+    }
+
+
+    function handleShift({ complete, index }) {
+        setTasks((prevTasks) => [
+            ...prevTasks,
+            complete
+        ])
+
+        handleCRemove(index)
+    }
+
   return (
-    <div className="App">
-        <div className="Title">
-          Task Management
-        </div>
-        
-        <AdditionBar></AdditionBar>
+    <div className="App">        
+            <div className="title">
+                Task Manager
+            </div>
+
+            <div>
+                <input type="text" value={inputValue} onChange={handleChange} id="task-input" />
+                {inputValue && <button onClick={handleClick} id="add-button ">+</button>}
+            </div>
+
+
+            <ul id="task-list">
+                {tasks.length === 0 && <div id="nothing-text">There's nothing here now, add a task!</div>}
+
+                {tasks.map((task, index) =>
+                    <li key={index} className="tasks">
+                        {task}
+
+                        <button id="done-btn" onClick={() => handleDone({ task, index })}>
+                            DONE
+                        </button>
+                        <button id="remove-btn" onClick={() => handleTRemove(index)}>
+                            REMOVE
+                        </button>
+                    </li>
+                )}
+            </ul>
+
+            <div className="title">
+                Completed
+            </div>
+
+            <ul id="completed-list">
+                {completed.map((complete, index) =>
+                    <li key={index} className="tasks">
+                        {complete}
+
+                        <button id="incomplete-btn" onClick={() => handleShift({ complete, index })}>
+                            INCOMPLETE
+                        </button>
+                        <button id="remove-btn" onClick={() => handleCRemove(index)}>
+                            REMOVE
+                        </button>
+                    </li>
+                )}
+            </ul>
+
+    )
     </div>
   );
 }
